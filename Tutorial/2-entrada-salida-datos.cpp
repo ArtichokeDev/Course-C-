c099f6b3b0c657eb2e6dc5fcae52823bca73b012
ArtichokeDev <artichok3dev@gmail.com>
2022-07-01 13:07:20 +0200

    course 2

---
+++ Tutorial/2-entrada-salida-datos.cpp
@@ -0,0 +1,22 @@
+//======================
+//  TUTORIAL C++
+//======================
+
+#include <iostream>
+
+using namespace std;
+
+//main
+int main(){
+
+    //flotante
+    float num;
+
+    cout << "Indica un numero: ";
+    cin >> num;
+    
+    //print
+    cout << num << endl;
+
+    return 0;
+}
---
+++ Tutorial/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+//======================
+//  TUTORIAL C++
+//======================
+
+//Librerias (#include<>)
+#include <iostream>
+
+using namespace std;
+
+//main
+int main(){
+
+    cout << "Hello World" << endl;
+
+    return 0; // return funcion (int main = "entero")
+}
